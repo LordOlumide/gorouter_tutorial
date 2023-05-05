e27b5d306c53b2dea357372609c470095f991d57
Olumide Olubosede <olumideolubosede@gmail.com>
2023-05-05 13:21:11 +0100

    simple setup

--- lib/app/screens/profile_screen.dart
+++ lib/app/screens/profile_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: const Text('Profile Screen'),
       ),
       body: const Center(
-        child: Text('Home screen'),
+        child: Text('Profile screen'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gorouter_tutorial/app/screens/home_screen.dart';
+import 'package:gorouter_tutorial/app/screens/profile_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      title: 'Go Router Demo',
+      routerConfig: _router,
     );
   }
+
+  final GoRouter _router = GoRouter(
+    // initialLocation: '/profile',
+    routes: [
+      GoRoute(path: '/', builder: ((context, state) => const HomeScreen())),
+      GoRoute(
+          path: '/profile',
+          builder: ((context, state) => const ProfileScreen())),
+    ],
+  );
 }
